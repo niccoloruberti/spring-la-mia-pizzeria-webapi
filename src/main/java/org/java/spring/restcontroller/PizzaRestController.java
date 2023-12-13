863a723a083a9325624c0980719dc6cb969a73e7
niccoloruberti <nikiruberti@gmail.com>
2023-12-13 14:29:39 +0100

    aggiunto parametro filtraggio pizze per nome

--- src/main/java/org/java/spring/restcontroller/PizzaRestController.java
+++ src/main/java/org/java/spring/restcontroller/PizzaRestController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,9 +22,17 @@ public class PizzaRestController {
 	private PizzaService pizzaService;
 	
 	@GetMapping
-	public ResponseEntity<List<Pizza>> getIndex() {
+	public ResponseEntity<List<Pizza>> getIndex(@RequestParam(required = false) String nome) {
 		
-		List<Pizza> pizzas = pizzaService.findAll();
+		List<Pizza> pizzas;
+		
+        if (nome != null && !nome.isEmpty()) {
+            
+            pizzas = pizzaService.findByNome(nome);
+        } else {
+            
+            pizzas = pizzaService.findAll();
+        }
 		
 		return new ResponseEntity<>(pizzas, HttpStatus.OK);
 	}
