89f80f52b86e346013e48a74b717fc024c29c0b0
niccoloruberti <nikiruberti@gmail.com>
2023-12-14 14:42:58 +0100

    vista lista pizze

--- front-end/pizza-frontend/src/App.vue
+++ front-end/pizza-frontend/src/App.vue
@@ -1,11 +1,28 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <PizzaIndex
+  :pizzas="pizzas"/>
 </template>
 
-<style scoped>
+<script setup>
+  //import libraries
+  import axios from 'axios';
+  import { onMounted, ref } from 'vue';
 
+  //import components
+  import PizzaIndex from './components/PizzaIndex.vue';
+
+  //data
+  const pizzas = ref(null);
+
+  const getPizzas = async () => {
+  const data = await axios.get("http://localhost:8080/api/v1.0/pizzas");
+  pizzas.value = data.data;
+  };
+
+  //hooks
+  onMounted(getPizzas);
+</script>
+
+<style scoped>
+  
 </style>
---
+++ front-end/pizza-frontend/src/components/PizzaIndex.vue
@@ -0,0 +1,32 @@
+<template lang="">
+    <h2 class="mb-5">Tutte le pizze:</h2>
+    <ul>
+        <li 
+            class="list-unstyled"
+            v-for="pizza in pizzas" 
+            :key="pizza.id"
+        >
+            {{ pizza.nome }}
+        </li>
+    </ul>
+
+</template>
+
+<script setup>
+    // import libraries
+    import { defineProps } from 'vue';
+
+    // props
+    const props = defineProps({
+        pizzas: {
+            type: Array,
+            required: true
+        }
+    });
+
+
+</script>
+
+<style lang="">
+    
+</style>
